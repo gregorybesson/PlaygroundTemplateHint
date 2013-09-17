51980fb3c5c154f7f4a52d9c8e948982a3711eab
GrG <gregory.besson@adfab.fr>
2013-09-17 09:46:11 +0200

    Graphic selection

--- src/PlaygroundTemplateHint/View/Renderer/PhpRendererHint.php
+++ src/PlaygroundTemplateHint/View/Renderer/PhpRendererHint.php
@@ -464,10 +464,10 @@ class PhpRendererHint implements \Zend\View\Renderer\RendererInterface, \Zend\Vi
                 ob_start();
                 //echo "<div style='background-color:#" . $color[$i]  . ";border: 1px solid black;' >";
                 //echo   "<a href='#' title = '". $this->__file ."'>></a><br/>";
-                echo "<!--Greg-100-->";
+                echo "<!--template-hint-->";
                 include $this->__file;
                 //echo "</div>";
-                echo "<!--Greg-100-->";
+                echo "<!--/template-hint-->";
                 $this->__content = ob_get_clean();
                 
             } catch (\Exception $ex) {
@@ -535,6 +535,46 @@ class PhpRendererHint implements \Zend\View\Renderer\RendererInterface, \Zend\Vi
     }
     
     /*
+     
+var tmpHint = 0;
+function ColorLuminance(hex, lum) {
+
+	// validate hex string
+	hex = String(hex).replace(/[^0-9a-f]/gi, '');
+	if (hex.length < 6) {
+		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+	}
+	lum = lum || 0;
+
+	// convert to decimal and change luminosity
+	var rgb = "#", c, i;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(hex.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+		rgb += ("00"+c).substr(c.length);
+	}
+
+	return rgb;
+};
+$("button").click(function () {
+    
+    var i = 1, tmpHint=false;
+	$('*')
+	.contents()
+	.each(function(){
+		if (this.nodeType === 8 && this.nodeValue == 'template-hint'){
+            tmpHint = true
+		} else if(tmpHint && this.nodeType ==1){
+			$(this).css('background-color', ColorLuminance("#69c", i));
+		}else if(this.nodeType === 8 && this.nodeValue == '/template-hint'){
+			tmpHint = false;
+            i=i/2;
+		}
+
+	});
+});
+     
+     
      
      #selector-top, #selector-bottom {
 background: black;
